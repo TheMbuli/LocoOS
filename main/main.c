f31adfd89c3ae1d22ddc267954123c6d5c47a698
EDDY MBULINYOLO <thembledpro@gmail.com>
2026-09-23 19:39:19 +0200

    chore: first boot in loco os

---
+++ main/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void app_main(void)
+{
+    printf("\n");
+    printf("============================\n");
+    printf("||       LocoOS BOOTING    ||  \n");
+    printf("============================\n");
+    printf(" LocoOS boot succefully ! \n");
+    printf("Hello from ESP32.\n");
+}
---
+++ remind.md
@@ -0,0 +1,4 @@
+# ACTIVER L'ENVIRONNEMENT VIERTUEL 
+
+export IDF_TOOLS_PATH=/mnt/main_disk4/esp-idf-tools
+source /mnt/main_disk4/espressif/v6.1/esp-idf/export.sh
